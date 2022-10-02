856795c5db7c2310089427880ea665b5bcfe7da6
Adam777 <Adam777-5867@outlook.com>
2022-10-02 22:20:56 +0000

    Update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -14,13 +14,13 @@ app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
   return res.json({greeting: 'hello API'});
 });
 
@@ -35,12 +35,12 @@ var listener = app.listen(process.env.PORT, function () {
 app.get('/api/:date_string?', function (req, res) {
   let date_input = req.params.date_string;
   let date = new Date();
-  
+
   if (date_input !== undefined) {
     if (isNumeric(date_input)) {
       date_input = parseInt(date_input);
     }
-    
+
     date = new Date(date_input);
   }
 
